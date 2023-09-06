b9df98537e409f5bcfe113cbdd093983fcf65c07
islamyehia1 <islamyehia1999@gmail.com>
2023-09-06 23:57:26 +0200

    feat: created fields

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,85 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import Image from "next/image";
+import S from "./page.module.scss";
+import SwapIcon from "./swap.svg";
+import Dropdown from "./Dropdown";
+
+import { useState, useEffect } from "react";
+const options = [
+  {
+    id: 0,
+    title: "New York",
+    selected: false,
+    key: "location",
+  },
+  {
+    id: 1,
+    title: "Dublin",
+    selected: false,
+    key: "location",
+  },
+  {
+    id: 2,
+    title: "California",
+    selected: false,
+    key: "location",
+  },
+  {
+    id: 3,
+    title: "Istanbul",
+    selected: false,
+    key: "location",
+  },
+  {
+    id: 4,
+    title: "Izmir",
+    selected: false,
+    key: "location",
+  },
+  {
+    id: 5,
+    title: "Oslo",
+    selected: false,
+    key: "location",
+  },
+];
 
 export default function Home() {
+  const [sourceCurrency, setSourceCurrency] = useState(null);
+  const [toCurrency, setToCurrency] = useState(null);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={S.main}>
+      <div className={S.wrapper}>
+        <div className={S.dropdown}>
+          <label htmlFor="sourceCurrency">From</label>
+          <Dropdown
+            onSelect={(selected: any) => setSourceCurrency(selected)}
+            list={options}
+            selected={sourceCurrency}
+            title={"Currency"}
+          />
+        </div>
+        <div className={S.dropdown}>
+          <label htmlFor="sourceCurrency">To</label>
+          <Dropdown
+            onSelect={(selected: any) => setToCurrency(selected)}
+            list={options}
+            selected={toCurrency}
+            title={"Currency"}
+          />
+        </div>
+        <div className={S.inputGroup}>
+          <label htmlFor="name">From:</label>
+          <input type="text" id="name" name="user_name" />
+        </div>
+        <button className={S.swapBtn}>
+          <SwapIcon />
+        </button>
+        <div className={S.inputGroup}>
+          <label htmlFor="name">To:</label>
+          <input type="text" id="name" name="user_name" />
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
